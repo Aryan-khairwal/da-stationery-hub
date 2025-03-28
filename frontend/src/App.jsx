f533edab25b21e15df7425ce725cd2623acfa94e
dpk1305 <dkseth2020@gmail.com>
2025-03-28 22:25:48 +0530

    api testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Card from './Components/Cards/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>hello world</h1>
+      <Card/>
     </>
   )
 }
